e4969658899b6e86387f4c7ab9022d94da60f1e3
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-07-29 15:02:27 -0400

    login completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
 import Articles from './Articles'
 import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from 'axios'
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
+
+
+const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    headers: {
+      authorization: token,
+    }
+});
+}
 
 export default function App() {
   // ✨ MVP can be achieved with these states
@@ -15,6 +27,11 @@ export default function App() {
   const [articles, setArticles] = useState([])
   const [currentArticleId, setCurrentArticleId] = useState()
   const [spinnerOn, setSpinnerOn] = useState(false)
+
+
+  useEffect(()=> {
+    setCurrentArticle(articles.filter(article => {return article.article_id === currentArticleId })[0]);
+  }, [currentArticleId])
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
@@ -27,6 +44,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    setMessage("Goodbye!")
+    if(!!localStorage.getItem('token'))localStorage.removeItem('token')
+    navigate('/')
   }
 
   const login = ({ username, password }) => {
@@ -36,6 +56,16 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    setMessage('');
+    setSpinnerOn(true);
+    axios.post('http://localhost:9000/api/login', { username, password })
+      .then(res => {
+        setSpinnerOn(false)
+        localStorage.setItem('token', res.data.token)
+        setMessage(res.data.message)
+        navigate('/articles')
+      })
+      .catch(err => console.log(err))
   }
 
   const getArticles = () => {
@@ -74,7 +104,7 @@ export default function App() {
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
         <nav>
-          console.log('Test')
+          
           <NavLink id="loginScreen" to="/">Login</NavLink>
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
